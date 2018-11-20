06f301d65a53ffa069fdc95d8e2bd8015bd0587f
Ramy Emad <theoutlier1@gmail.com>
2018-11-20 16:03:13 +0200

    Add New User Migrate

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
 # Create your models here.
+class Note(models.Model):
+       user = models.ForeignKey(User,on_delete=models.CASCADE)
+       title =   models.CharField(max_length=50)
+       slug =    models.SlugField()
+       content =models.TextField(blank=True)
+       created = models.DateTimeField(blank=True,default=datetime.datetime.now)
+       active =  models.BooleanField(default=True)
+       tage = models.CharField(blank=True,max_length=100)
--- outline.md
+++ outline.md
@@ -1,1 +1,8 @@
 Create Model -Note App
+- user
+- title
+- content
+- date time
+- tags
+- active
+- slug
